33c9b70a8c03e864ca4c26a4bb291ffe2477d48c
Hongxiang Fan <hf17@ccgpu3.doc.ic.ac.uk>
2023-09-12 23:30:24 +0100

    at:5 cm scripts

--- script/reproduce-paper-m2023-5/run.bat
+++ script/reproduce-paper-m2023-5/run.bat
@@ -1,12 +1,4 @@
-@echo off
+rem native script
 
-set CUR_DIR=%cd%
-
-echo.
-echo Current execution path: %CUR_DIR%
-echo Path to script: %CM_TMP_CURRENT_SCRIPT_PATH%
-echo ENV CM_EXPERIMENT: %CM_EXPERIMENT%
-
-echo.
-%CM_PYTHON_BIN_WITH_PATH% %CM_TMP_CURRENT_SCRIPT_PATH%\main.py
-IF %ERRORLEVEL% NEQ 0 EXIT %ERRORLEVEL%
+echo "Windows is not supported yet"
+exit /b 1
--- script/reproduce-paper-m2023-5/run.sh
+++ script/reproduce-paper-m2023-5/run.sh
@@ -1,12 +1,33 @@
-#!/bin/bash
+echo "====================== Atifacts Evaluation for MICRO23 paper=========================="
+echo "Sparse-DySta: Sparsity-Aware Dynamic and Static Scheduling for Sparse Multi-DNN Workloads"
+container="docker"
 
-CUR_DIR=${PWD}
+echo "==================  Run a container test to make sure container works =================="
+${container} run docker.io/hello-world
 
-echo ""
-echo "Current execution path: ${CUR_DIR}"
-echo "Path to script: ${CM_TMP_CURRENT_SCRIPT_PATH}"
-echo "ENV CM_EXPERIMENT: ${CM_EXPERIMENT}"
+echo "====================================================================================="
 
-echo ""
-${CM_PYTHON_BIN_WITH_PATH} ${CM_TMP_CURRENT_SCRIPT_PATH}/main.py
+echo "==================  Pulling the Docker image to run the experiments =================="
+${container} pull hxfan/spar-dysta-micro23:ae
+
+echo "==================  Creating Container to run the experiments =================="
+sudo ${container} run -it -d --name spar-dysta --gpus all  hxfan/spar-dysta-micro23:ae /bin/bash # Create container
+
+
+echo "================== Generate Figure-12, Attention  =================="
+sudo ${container} exec --workdir /workspace/dysta-sparse/dysta_scheduler spar-dysta script/attnn/dysta_comparison_sanger_tradeoff_analysis.sh
+echo "================== Generate Figure-12, CNN  =================="
+sudo ${container} exec --workdir /workspace/dysta-sparse/dysta_scheduler spar-dysta script/cnn/dysta_comparison_eyerissv2_tradeoff_analysis.sh
+
+echo "================== Generate Figure-13, Attention  =================="
+sudo ${container} exec --workdir /workspace/dysta-sparse/dysta_scheduler spar-dysta script/attnn/effect_sparsity_sanger.sh
+echo "================== Generate Figure-13  CNN =================="
+sudo ${container} exec --workdir /workspace/dysta-sparse/dysta_scheduler spar-dysta script/cnn/effect_sparsity_eyerissv2.sh 
+
+
+echo "================== Generate Table5 & Figure-14, Attention  =================="
+sudo ${container} exec --workdir /workspace/dysta-sparse/dysta_scheduler spar-dysta script/attnn/dysta_comparison_sanger_across_slo.sh
+echo "================== Generate Table5 & Figure-14  CNN =================="
+sudo ${container} exec --workdir /workspace/dysta-sparse/dysta_scheduler spar-dysta script/cnn/dysta_comparison_eyerissv2_across_slo.sh
+
 test $? -eq 0 || exit 1
