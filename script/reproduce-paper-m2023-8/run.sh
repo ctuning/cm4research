e7c863156915cad518aa9841b6c11a485b97a1da
Nandeeka Nayak <nandeeka@gmail.com>
2023-09-12 19:25:50 -0500

    Add missing directory change

--- script/reproduce-paper-m2023-8/run.sh
+++ script/reproduce-paper-m2023-8/run.sh
@@ -7,6 +7,7 @@ echo "Current execution path: ${CUR_DIR}"
 echo "Path to script: ${CM_TMP_CURRENT_SCRIPT_PATH}"
 
 echo "${CM_GIT_REPO_FPSG_UIUC_TEAAL_CHECKOUT_PATH}"
+cd ${CM_GIT_REPO_FPSG_UIUC_TEAAL_CHECKOUT_PATH}
 
 docker-compose run cl scripts/run.sh
 
